0ea6446f4a3a0f113a4f3ec2368442bc854376fb
Annopaolo <arnaldo.cesco@gmail.com>
2020-08-26 13:32:17 +0200

    Cleaned code for RDD

--- src/main/scala/RDDAnalysis.scala
+++ src/main/scala/RDDAnalysis.scala
@@ -44,55 +44,69 @@ object RDDAnalysis {
     val phrases = book.select("ChapterData");
     val pipeline2 = Utils.getSentimentPipeline
 
-    val trans = pipeline2.annotate(phrases, "ChapterData");
-
-    val arrs = trans.select(
+    val raw = pipeline2.annotate(phrases, "ChapterData").select(
       $"sentiment.result".as("sentiment"),
       $"sentence.result".as("sentence")
     );
 
-    val phr = arrs.
+    val data = raw.
       withColumn("r", explode(arrays_zip($"sentiment", $"sentence")))
       .select(
         $"r.sentiment" as("sentiment"),
         $"r.sentence" as("sentence"))
 
-    val myFavouriteRdd = phr.rdd.map(r => (r.getString(0), r.getString(1)));
+    val base = data.rdd.map(r => (r.getString(0), r.getString(1)));
 
     val namesBroadcast = spark.sparkContext.broadcast(names);
 
-    val parRdd2 = myFavouriteRdd.zipWithUniqueId()
+    val sentWordAndId = base.zipWithUniqueId()
                     .flatMap{case ((sentiment, sentence),id) =>
                       sentence.replaceAll("[,.!?:;]", "")
                                .split(" ")
                                 .map(e => ((Utils.sentToInt(sentiment),e), id))
-                    }.filter{case ((_, word),_) => namesBroadcast.value contains word}
+                    }
+                    .filter{case ((_, word),_) => namesBroadcast.value contains word}
+                    //((sent, name), id)
 
-    val rddForDf = parRdd2.map(x => (x._2, x._1)).cache()
+    val IdAndSentWord = sentWordAndId.map(x => (x._2, x._1)).cache()
 
-    val rddForTf = rddForDf.groupByKey()
-                           .map{case (id,other) =>
-                             (id, (other.head._1, other.map(
-                               e => e._2
-                             ) toList))
-                           }
+    val rddForTf = IdAndSentWord
+                    .groupByKey()                   //HERE! -- too difficult?
+                    .map{case (id,other) =>
+                      (id, (other.head._1, other.map(
+                        e => e._2
+                        )
+                        toList))
+                    }
+                    //(id, (sent, [names])
 
-    val docs = rddForTf.count().toInt
+    val docs = rddForTf.count().toInt //TODO: use the actual document number
     val docCount = spark.sparkContext.broadcast(docs)
-
     println(s"There are ${docCount.value} documents to be considered.")
-    println("----------SOME DF---------")
 
-    val realRddForDf = rddForDf.map(x => (x._1, x._2._2));
-    val df = realRddForDf.map(x => (x._2, x._1)).distinct().groupByKey().map(x => (x._1, x._2.size))//.sortBy(x => x._2);
-    df.take(10).foreach(x => println(s"${x._1}, ${x._2}"))
+    println(s"----------------DF-----------------------")
+    val rddForDf = IdAndSentWord.map(x => (x._1, x._2._2));
+    val df = rddForDf.map(x => (x._2, x._1))
+                      .distinct()
+                      .groupByKey()               //HERE!
+                      .map(x => (x._1, x._2.size))
+/*                      .map{case (name, _) => ((name,1))}
+                      .reduceByKey(_+_)*/
+                      //(name, df)
+    df.sortBy(x => x._2).take(namesBroadcast.value.length).foreach(x => println(s"${x._1}, ${x._2}"))
 
-    //TODO: see if it's better compute idf here
-    //TODO: try if it's better to keep indexes and join with sentiment
-    val tf = rddForTf.map{case (i,(sent, names)) => (i, (sent, listToMap(names) toSeq))}
-    //tf.take(10).foreach(x => println(s"${x._1}, sent: ${x._2._1}, ${x._2._2.mkString(",")}"))
-    val justTf = tf.map{x => x._2}.flatMap{case (sent,vals) => vals.map(e => (sent, e))}
-    //justTf.take(10).foreach(x => println(s"sent: ${x._1}, occurrences: ${x._2}"))
+    //TODO: try if it's better to keep indexes and join with sentiment  -- it is not! Join costs more
+    val namesMapInSentence = rddForTf.map{case (i,(sent, names)) => (i, (sent, listToMap(names) toSeq))}
+    //(id,(sent, [(namei,tfi)])
+    //namesMapInSentence.take(10).foreach(x => println(s"${x._1}, sent: ${x._2._1}, ${x._2._2.mkString(",")}"))
+    val tf = namesMapInSentence
+              .map{x => x._2}
+              .flatMap{
+                case (sent,vals) =>
+                  vals.map(e => (sent, e))
+              }
+    //(sent, (namei,tfi))
+    //tf.take(10).foreach(x => println(s"sent: ${x._1}, occurrences: ${x._2}"))
 
     def computeResult (in : RDD[(String, Int)], computationName : String) : Unit = {
       println(s"-----------$computationName--------------")
@@ -102,10 +116,12 @@ object RDDAnalysis {
       sumUp.foreach(x => println(s"${x._1}, ${x._2}"))
     }
 
-    val freqs = justTf.map{case (_, (name, freq)) => (name, freq)}
+    val freqs = tf.map{case (_, (name, freq)) => (name, freq)}
+    //(namei,tfi)
     computeResult(freqs, "simple tf-idf")
 
-    val tfPlusSent = justTf.map{case (sent, (name, freq)) => (name, sent * freq)}
+    val tfPlusSent = tf.map{case (sent, (name, freq)) => (name, sent * freq)}
+    //(namei,tfi*sent)
     computeResult(tfPlusSent, "tf-idf + sentiment")
   }
 
