c96e81ccb273d26108217fb98ed27dc0b8849ead
Annopaolo <arnaldo.cesco@gmail.com>
2020-09-03 19:41:11 +0200

    print?

--- src/main/scala/LotRAnalysis.scala
+++ src/main/scala/LotRAnalysis.scala
@@ -7,7 +7,7 @@ object LotRAnalysis {
 
   val spark: SparkSession = SparkSession.builder()
     .appName("LotRAnalysis")
-    .master("local[*]")
+    //.master("local[*]")
     .getOrCreate()
 
 
@@ -20,14 +20,22 @@ object LotRAnalysis {
       case _ => throw new IllegalArgumentException("Command-line arguments parse error")
     }
     spark.sparkContext.stop()
+    //toFile (prettify(res._1, "tfidf"), Utils.outputPath)
+    //toFile (prettify(res._2, "tfidf*sentiment"), Utils.outputPath)
+
     print (prettify(res._1, "tfidf"))
     print (prettify(res._2, "tfidf*sentiment"))
   }
 
-  def prettyPrint(in : Array[(String, Double)], name : String) : Unit = {
-    println(s"-----------$name---------------")
-    in.sortBy(_._2)foreach(x => println(f"${x._1}%s : ${x._2}%.4f"))
+  def toFile(from: String, to: String) = {
+    import java.io._
+    val file = new File(to)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.append(from)
+    bw.flush()
+    bw.close()
   }
+
   def prettify(in : Array[(String, Double)], name : String) : String = {
     val l = s"-----------$name---------------" :: (in.sortBy(_._2).map(x => f"${x._1}%s : ${x._2}%.4f")).toList
     l
