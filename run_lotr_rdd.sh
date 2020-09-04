d2ff85a1e7ce6d014a54fe736c4aa299de13e9ab
Annopaolo <arnaldo.cesco@gmail.com>
2020-09-04 20:14:40 +0200

    Delete run_lotr_rdd.sh

--- run_lotr_rdd.sh
+++
@@ -1,8 +0,0 @@
-spark-submit \
---packages com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.5 \
-lotr_2.11-0.1.jar \
-rdd \
-s3://lotr-analysis-try/LordOfTheRingsBook.json \
-s3://lotr-analysis-try/analyze_sentiment_en_2.4.0_2.4_1580483464667/
-
-
